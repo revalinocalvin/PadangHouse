c03a731c7b124da64390cd3d306d29d93f583859
revalinocalvin <revalinocalvin@gmail.com>
2024-05-08 09:25:23 +0700

    Item Submitting Added
    Changed Customer Pathing script

---
+++ Assets/Scripts/Object/ObjectSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn; 
+    public int maxInteractions = 5; 
+    private int interactionCount = 0; 
+
+    public GameObject SpawnObject()
+    {
+        if (interactionCount < maxInteractions)
+        {
+            GameObject spawnedObject = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            interactionCount++; 
+            if (interactionCount >= maxInteractions)
+            {
+                Destroy(gameObject); 
+            }
+
+            return spawnedObject;
+        }
+        else
+        {
+            Debug.Log("Object spawner has reached its maximum usage.");
+            return null; 
+        }
+    }
+}
