db39fefe09b4039b0e927916fcd6975aa59a90fe
revalinocalvin <revalinocalvin@gmail.com>
2024-05-15 00:09:25 +0700

    Enchanced Player Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public Rigidbody2D rb;
+    public GameObject gp;
+    public GameObject rp;
+    public GameObject eye;
     private Vector2 moveDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gp = GameObject.Find("Player/GrabPosition");
+        rp = GameObject.Find("Player/RayPosition");
+        eye = GameObject.Find("Player/Eye");
     }
     
     void Update()
@@ -21,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        Direction();
     }
 
     void ProcessInputs()
@@ -32,7 +39,40 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);   
+    }
+
+    void Direction()
+    {
+        if (moveDirection.x > 0)
+        {
+            Debug.Log("Right");
+            rp.transform.localPosition = new Vector2(1.0f, 0.0f);
+            gp.transform.localPosition = new Vector2(0.7f, 0.0f);
+            eye.transform.localPosition = new Vector2(0.216f, 0.585f);
+        }
+
+        else if (moveDirection.x < 0)
+        {
+            Debug.Log("Left");
+            rp.transform.localPosition = new Vector2(-1.0f, 0.0f);
+            gp.transform.localPosition = new Vector2(-0.7f, 0.0f);
+            eye.transform.localPosition = new Vector2(-0.216f, 0.585f);
+        }
+
+        else if (moveDirection.y > 0)
+        {
+            Debug.Log("Up");
+            rp.transform.localPosition = new Vector2(0.0f, 1.3f);
+            gp.transform.localPosition = new Vector2(0.0f, 1.2f);
+        }
+
+        else if (moveDirection.y < 0)
+        {
+            Debug.Log("Down");
+            rp.transform.localPosition = new Vector2(0.0f, -1.3f);
+            gp.transform.localPosition = new Vector2(0.0f, -1.2f);
+        }
     }
 }
 
