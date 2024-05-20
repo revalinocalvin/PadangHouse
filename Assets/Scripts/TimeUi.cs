b592c154eda509416846cef6151ebc98fcebff8d
revalinocalvin <revalinocalvin@gmail.com>
2024-05-21 01:21:01 +0700

    Added Time Manager and Time Ui (1 Hour in game = 30 Seconds)

---
+++ Assets/Scripts/TimeUi.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeUi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public TextMeshProUGUI timeText;
+
+    private void OnEnable()
+    {
+        TimeManager.OnMinuteChanged += UpdateTime;
+        TimeManager.OnHourChanged += UpdateTime;
+    }
+    
+    private void OnDisable()
+    {
+        TimeManager.OnMinuteChanged -= UpdateTime;
+        TimeManager.OnHourChanged -= UpdateTime;
+    }
+
+    private void UpdateTime()
+    {
+        timeText.text = $"{TimeManager.Hour:00}:{TimeManager.Minute:00}";
+    }
+}
